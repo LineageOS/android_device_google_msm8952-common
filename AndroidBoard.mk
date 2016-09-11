5bbaa1c60b83b5269fc92744e2cc6be9c782cdf9
Kaan Külahlı <kaankulahli@gmail.com>
2016-09-12 02:51:38 +0300

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,3 +4,7 @@ LOCAL_PATH := $(call my-dir)
 # extra images
 #----------------------------------------------------------------------
 include build/core/generate_extra_images.mk
+
+ifeq ($(TARGET_BOARD_PLATFORM),msm8k)
+    LOCAL_CFLAGS += -DTARGET8x52
+endif
