98cef5340931bef618cd42a10faafe57e68d3002
Caner Aydın <punisher_caner@windowslive.com>
2016-09-15 05:41:41 +0300

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,7 +4,3 @@ LOCAL_PATH := $(call my-dir)
 # extra images
 #----------------------------------------------------------------------
 include build/core/generate_extra_images.mk
-
-ifeq ($(TARGET_BOARD_PLATFORM),msm8k)
-    LOCAL_CFLAGS += -DTARGET8x52
-endif
